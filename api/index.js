e1698a11a494b2aa7bb7b524c8300ab12a8d798b
itzhangbao <itzhangbao@163.com>
2021-05-08 10:46:15 +0800

    mac commit

--- api/index.js
+++ api/index.js
@@ -8,28 +8,28 @@ var mustache = require('mustache');
 module.exports = (req, res) => {
     // const { name = 'World' } = req.query
     // res.send(`Hello ${name}!`)
-  res.send(__dirname + '/template.plist');
+  // res.send(__dirname + '/template.plist');
   // res.send("res"+res.query);
   //   console.log("res"+res.query);
   //   console.log("plist"+path.join(__dirname, '..') + '/template.plist');
 
   // fs.readFile(path.join(__dirname, '..') + '/template.plist', function(err, data) {
-  // // fs.readFile(__dirname + '/template.plist', function(err, data) {
-  //   if (err)
-  //     throw err;
-  //   var template = data.toString();
+  fs.readFile(__dirname + '/template.plist', function(err, data) {
+    if (err)
+      throw err;
+    var template = data.toString();
 
-  //   var encodedName = req.query.name;
-  //   var name = base64.decode(encodedName)
-  //   var rendered = mustache.render(template, {
-  //     encodedName: encodedName,
-  //     name: name,
-  //     ip: "ipAddress",
-  //     port: "port",
-  //   });
+    var encodedName = req.query.name;
+    var name = base64.decode(encodedName)
+    var rendered = mustache.render(template, {
+      encodedName: encodedName,
+      name: name,
+      ip: "ipAddress",
+      port: "port",
+    });
 
-  //   res.set('Content-Type', 'text/plain; charset=utf-8');
-  //   res.send(rendered);
-  // })
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send(rendered);
+  })
   
 }
