a786fef1737d1c02bb3d53573e020f31fc319631
itzhangbao <itzhangbao@163.com>
2021-05-08 11:08:13 +0800

    mac commit

--- api/index.js
+++ api/index.js
@@ -6,14 +6,7 @@ var base64 = require('base64-url');
 var mustache = require('mustache');
 
 module.exports = (req, res) => {
-    // const { name = 'World' } = req.query
-    // res.send(`Hello ${name}!`)
-  // res.send(__dirname + '/template.plist');
-  // res.send("res"+res.query);
-  //   console.log("res"+res.query);
-  //   console.log("plist"+path.join(__dirname, '..') + '/template.plist');
 
-  // fs.readFile(path.join(__dirname, '..') + '/template.plist', function(err, data) {
   fs.readFile(__dirname + '/template.plist', function(err, data) {
     if (err)
       throw err;
@@ -23,16 +16,14 @@ module.exports = (req, res) => {
 
     var name = base64.decode(encodedName)
 
-    res.send(name);
-    // var rendered = mustache.render(template, {
-    //   encodedName: encodedName,
-    //   name: name,
-    //   ip: "ipAddress",
-    //   port: "port",
-    // });
+    var rendered = mustache.render(template, {
+      encodedName: encodedName,
+      name: "小杜",
+      ipaUrl: "http://127.0.0.1/NoSign/22222222222.ipa",
+    });
 
-    // res.set('Content-Type', 'text/plain; charset=utf-8');
-    // res.send(rendered);
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send(rendered);
   })
   
 }
